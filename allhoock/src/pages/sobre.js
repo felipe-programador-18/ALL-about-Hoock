f73bccb2581be791c23175ae62e21943115d76fd
Felipe <machadofelipe2016@outlook.com>
2022-07-22 16:58:57 -0300

    c12-adding personalities hoock there and practice destructing here too

--- allhoock/src/pages/sobre.js
+++ allhoock/src/pages/sobre.js
@@ -1,12 +1,31 @@
 import AddingSome from "../Componente/useeeffect"
+import { useVerify } from "../Personalitehook/useContext"
+import {useVerifyColor} from "../Personalitehook/useColors"
+
 
 const Sobre = ( ) => {
-    return (<>
-         
+   const{count, setCount} = useVerify()
+   console.log('testing here', count) 
+   
+   const {color,dispatch} = useVerifyColor()
+   console.log("i am here", color) 
+   
+   const tradeColor = (color) => {
+     dispatch({type:color})
+   }
+  
+  
+  return (<>
+         <div style={{background:color}} >
 
    <p>sobre</p>
-   <p>sobre more about sobre</p>
+   <p style={{background:color}} >sobre more about sobre</p>
+     {count}
+     <button onClick={() => setCount(count+1) } >Increased more one</button>
+     <button onClick={ () =>  tradeColor("BLACK") } >black</button>
+     <button onClick={ () =>  tradeColor("BLUE") } >blue</button>
 
+      </div>
     </>)
 }
 
