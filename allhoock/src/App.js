50e7a28c2c1c38851428374c30e5b19baf7455b7
Felipe <machadofelipe2016@outlook.com>
2022-07-22 17:01:09 -0300

    c13-rearrange code about principle page

--- allhoock/src/App.js
+++ allhoock/src/App.js
@@ -2,11 +2,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import AboutAdding from './pages/about';
-
 import Home from './pages/home';
 import Sobre from './pages/sobre';
 
+
+
 function App() {
+    
+  
   return (<div className='App'>
   <AboutAdding/>
  <Routes>
