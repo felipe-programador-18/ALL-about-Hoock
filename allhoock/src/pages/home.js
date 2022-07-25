716e2bf33d9ea640627550c6ab8a7c4cabf35cdf
Felipe <machadofelipe2016@outlook.com>
2022-07-25 13:13:36 -0300

    c16-adding personalities hoock

--- allhoock/src/pages/home.js
+++ allhoock/src/pages/home.js
@@ -7,13 +7,29 @@ import PracticeMemo from '../Componente/useMemory'
 import CreateUseLayout from '../Componente/useLayoutmore'
 import AddingImperative from '../Componente/useImperative'
 import { useVerify } from '../Personalitehook/useContext'
+import { useVerifyColor } from '../Personalitehook/useColors'
 
 const Home = () => {
- 
+  const {color,dispatch} = useVerifyColor()
+  console.log('testing in the color', color)
+
   const {count, setCount} = useVerify()
-    
-    return ( <>
+     
+  
+  const tradecolor = (color) => {
+    dispatch({type:color})
+  }
+
+
+   
+    return ( <> 
+       <div style={{background:color}} >
            <h1>Adding home here and adding component that contains useEffect </h1>
+          {color}
+           <button onClick={() => tradecolor({color:'BLUE'})} > Trade another color </button>
+           <button onClick={() =>  tradecolor({color:'BLACK'})} >
+            Trade outer collors
+           </button>
             {count}
              <AddingSome/>
              <AddReffered/>
@@ -26,6 +42,7 @@ const Home = () => {
               <br/>
              <CreateUseLayout/>
              <AddingImperative/>
+        </div>
     </> )
 }
 
