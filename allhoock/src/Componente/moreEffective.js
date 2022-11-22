8542786b02c5cd09a8dba36871178f5afdc38060
Felipe <machadofelipe2016@outlook.com>
2022-11-22 12:27:21 -0300

    c28-practice a little bit useEffect

---
+++ allhoock/src/Componente/moreEffective.js
@@ -0,0 +1,21 @@
+import React,{useEffect, useState} from 'react'
+
+
+const practiceEffect = () => {
+        
+
+    //practice a little about this!
+    useEffect(() => {
+
+    },[])
+
+
+    return(
+     <>
+        <h1>Practice more and more about that!!</h1>
+     </>
+    )
+}
+
+
+export default practiceEffect
