218c923c03a67ac74f5ab8e3cbac1236afeb3843
Felipe <machadofelipe2016@outlook.com>
2022-11-22 13:00:12 -0300

    c30-practic a bit about useeffect

--- allhoock/src/Componente/moreEffective.js
+++ allhoock/src/Componente/moreEffective.js
@@ -3,16 +3,26 @@ import React,{useEffect, useState} from 'react'
 
 const PracticeEffect = () => {
     const[number, setNumber] = useState(0)        
+    
 
+
+    const testIncreased = () => {
+        setNumber(number+1)
+    }
     //practice a little about this!
     useEffect(() => {
-       setNumber(number+1) 
+       if(number>0){
+         console.log("the numbers its change more on time")
+       }
     },[number])
 
 
     return(
-     <>
+     <> 
         <h1>Practice more and more about that!!</h1>
+        {number}
+        <button onClick={() =>testIncreased()} >Increased</button>
+
      </>
     )
 }
