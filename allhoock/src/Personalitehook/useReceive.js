7141b0ba6ce435b7fe42266e4a36bb6485d08202
Felipe <machadofelipe2016@outlook.com>
2022-11-26 12:11:31 -0300

    c49-trade my name

--- allhoock/src/Personalitehook/useReceive.js
+++ allhoock/src/Personalitehook/useReceive.js
@@ -1,8 +1,7 @@
 import React, {useContext} from 'react'
 import { ManageContext } from '../hoock/anotherContext'
 
-export const useReceive= () =>{
-   
+export const useTesting= () =>{
     const authTest = useContext(ManageContext)
     if(!authTest){
      return  console.log("testing my context here broo, not have context here")
