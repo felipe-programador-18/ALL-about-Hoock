a603f0f6badce085f4eb303c09f5e11df98c883e
Felipe <machadofelipe2016@outlook.com>
2022-11-26 11:48:23 -0300

    c43-rearrange code

--- allhoock/src/Personalitehook/useReceive.js
+++ allhoock/src/Personalitehook/useReceive.js
@@ -1,14 +1,13 @@
-import {useContext} from 'react'
+import React, {useContext} from 'react'
 import { ManageContext } from '../hoock/anotherContext'
 
-export const useReceivePerso = () =>{
+export const useReceive= () =>{
    
     const auth = useContext(ManageContext)
 
     if(!auth){
-       return  console.log("testing my context here broo, not have context here")
+     return  console.log("testing my context here broo, not have context here")
     }
     return auth
 }
 
-export default useReceivePerso
