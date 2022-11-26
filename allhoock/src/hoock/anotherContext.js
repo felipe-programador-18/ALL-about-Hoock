049eff194a8e6189dbc8ee55f57d6636c43a47c2
Felipe <machadofelipe2016@outlook.com>
2022-11-26 12:11:12 -0300

    c48-rearrange my coding

--- allhoock/src/hoock/anotherContext.js
+++ allhoock/src/hoock/anotherContext.js
@@ -3,10 +3,10 @@ import React,{createContext,useState} from 'react'
 export const ManageContext =  createContext()
 
 export const ManageAll = ({children}) =>{   
-    const [counter, setCounter] = useState(0)
+    const [counter, setCounter] = useState(2)
   
     return <ManageContext.Provider value={{counter, setCounter}} >
         {children}
-    </ManageContext.Provider>
+       </ManageContext.Provider>
 
 }
