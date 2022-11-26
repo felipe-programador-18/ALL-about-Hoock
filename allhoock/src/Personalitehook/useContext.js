ff3ab7f3ac2b1107431e3825f608510f39c6b3dc
Felipe <machadofelipe2016@outlook.com>
2022-11-26 11:49:12 -0300

    c44-set up my context to check my state manage

--- allhoock/src/Personalitehook/useContext.js
+++ allhoock/src/Personalitehook/useContext.js
@@ -4,7 +4,6 @@ import { creatManageContext } from '../hoock/context'
 
 export const useVerify = () => { 
    const auth = useContext(creatManageContext)
-   
    if(!auth){
     return console.log('not have context here')
    }
