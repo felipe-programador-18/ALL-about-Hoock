3d7cc11f66d29d31620870b33810efce11ac50ff
Felipe <machadofelipe2016@outlook.com>
2022-11-26 10:51:28 -0300

    c37-practice more about that

--- allhoock/src/Personalitehook/useReceive.js
+++ allhoock/src/Personalitehook/useReceive.js
@@ -1,7 +1,18 @@
-import React from 'react'
-
+import React,{useContext} from 'react'
+import { ManageContext } from '../hoock/anotherContext'
 
 const ReceivePerso = () =>{
+ 
+    const {counter, setCounter} = useContext(ManageContext)
+
+    return( 
+        <>
+          <h1>Testing my hook about couter</h1>
+          {counter}
+
+          <button onClick={()=> setCounter(counter+1) } >+1</button> 
+        </>
+    )
 
 }
 
